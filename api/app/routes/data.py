1376a209dbe6a53357deb28ac9dfab8696a7f104
rafaellichen <rafaellichen@gmail.com>
2024-04-18 15:16:24 -0700

    correct json validation error

--- api/app/routes/data.py
+++ api/app/routes/data.py
@@ -1,12 +1,6 @@
-from app.constants import (
-    DATABASE_503_RESPONSE,
-    DEFAULT_QUERY_LIMIT,
-    MAX_QUERY_LIMIT,
-    d7validate,
-)
+from app.constants import DATABASE_503_RESPONSE, DEFAULT_QUERY_LIMIT, d7validate
 from app.core.subset import Subset
 from app.decorators import require_role
-from app.enums.search_mode import VerificationSearchMode
 from app.flask_app import app, project
 from app.routes.json_validation.base import BaseValidation
 from flask import abort, jsonify, make_response, request
@@ -192,7 +186,7 @@ def batch_update_metadata():
                         "type": "object",
                         "properties": {
                             "uuid": BaseValidation.uuid,
-                            "value": {"type": ["string", "array", "integer", "float"]},
+                            "value": {"type": ["string", "array", "number"]},
                         },
                     },
                 },
